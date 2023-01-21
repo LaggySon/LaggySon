15ba5e0c587bdc0eae2025e8cb52bcccc617809a
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-01-21 17:26:35 -0500

    train timer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen">
+	<h1 class="font-bold  text-yellow-500 text-center  text-7xl">LaggySon</h1>
+	<ul
+		class=" mt-9 font-DotMatrix bg-black text-yellow-500 w-100 lg:w-1/2 m-auto flex flex-col border-4 border-gray-400 p-4 bg-black relative"
+	>
+		<li class="space-x-4">
+			<a
+				href="https://scoreboardish.laggi.sh"
+				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
+				>Scoreboardish</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://tranquility.gg"
+				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
+				>Tranquility Gaming Website</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://github.com/laggyson"
+				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
+				>GitHub</a
+			>
+		</li>
+	</ul>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			fontFamily: {
+				DotMatrix: ['DotMatrix', 'sans-serif']
+			}
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
