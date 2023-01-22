52ccc725acad9f4755aa6b8b1a0751fb0662f492
LaggySon <56988314+LaggySon@users.noreply.github.com>
2023-01-22 11:46:59 -0500

    Laggish

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,46 @@
-<head><title>LaggySon</title></head>
+<head><title>Laggish</title></head>
 <div class="h-screen">
-	<h1 class="font-bold  text-yellow-500 text-center  text-7xl">LaggySon</h1>
+	<div
+		class="w-100 lg:w-1/2 m-auto bg-yellow-500 drop-shadow-lg border-t-0 uppercase mt-4 font-bold"
+	>
+		<h1 class="font-bold  text-yellow-500 text-center  text-7xl text-white">Laggish</h1>
+		<h2 class="text-center mt-3 text-3xl bg-white pb-2 whitespace-nowrap">
+			esports bits and peices
+		</h2>
+	</div>
+
 	<ul
-		class=" mt-9 font-DotMatrix bg-black text-yellow-500 w-100 lg:w-1/2 m-auto flex flex-col border-4 border-gray-400 p-4 bg-black relative"
+		class=" drop-shadow-lg mt-9 font-DotMatrix  text-yellow-500 w-100 lg:w-1/2 m-auto flex flex-col border-4 border-gray-400 p-4 bg-black relative"
 	>
 		<li class="space-x-4">
 			<a
 				href="https://scoreboardish.laggi.sh"
-				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
+				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-2xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
 				>Scoreboardish</a
 			>
 		</li>
 		<li>
 			<a
 				href="https://tranquility.gg"
-				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
-				>Tranquility Gaming Website</a
+				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-2xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
+				>Tranquility Gaming</a
 			>
 		</li>
 		<li>
 			<a
 				href="https://github.com/laggyson"
-				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
+				class="pl-2 hover:before:content-['>'] before:absolute before:left-2  flex justify-between text-2xl after:content-['ON-TIME'] hover:after:content-['ALL_ABOARD'] after:text-right after:whitespace-nowrap lg:text-3xl"
 				>GitHub</a
 			>
 		</li>
 	</ul>
 </div>
+
+<style>
+	:root {
+		background-image: url('/trainStation.jpg');
+		background-size: cover;
+		background-attachment: fixed;
+		background-position: center;
+	}
+</style>
